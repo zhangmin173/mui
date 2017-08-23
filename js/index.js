cad6d9f063f90114e06f0602b70e91606fa853e0
zhangmin <303890562@qq.com>
2017-08-23 15:23:19 +0800

    1

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ function Index() {
 }
 
 Index.prototype = {
+    w: null,
     init: function() {
         var _this = this;
 
@@ -21,13 +22,42 @@ Index.prototype = {
     render: function() {
         var _this = this;
 
+        _this.w = $('#j-con').WaterFall();
+        $('#j-con').MobileLoad({
+            debug: debug,
+            type: 'post',
+            url: '/tp5/index/index/page',
+            model: 'tpl-note',
+            initNoData: _this.initNoData,
+            noData: _this.noData,
+            errorData: '',
+            queryParams: function () {
+                return { 
+                    status: 1
+                }
+            },
+            onRender: function(e,d) {
+                var html = template(e.opts.model,d);
+                e.$element.append(html);
+                e.$element.imagesLoaded().always(function(instance) {
+                    _this.w.fall();
+                }).progress(function(instance, image) {
+                    if (image.isLoaded == false) {
+                        image.img.src = '/data/src/error.jpg';
+                    }
+                });
+            }
+        })
         _this.handle();
 
     },
     handle: function() {
         var _this = this;
 
-
+        $('body').on('click','.note-del',function() {
+            var i = $(this).parents('.note').index();
+            _this.w.remove(i);
+        })
     }
 };
 
